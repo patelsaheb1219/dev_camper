f2ee556e97fd1e1690ed100e4582bbd6f696aff6
patelsaheb1219 <kaumik1219@gmail.com>
2021-04-23 19:35:26 +0530

    WIP: Connect with the MongoDB database using mongoose npm package

--- server.js
+++ server.js
@@ -1,25 +1,41 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgon = require('morgan');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgon = require("morgan");
+const connectDB = require("./config/db");
 
 // Routes files
-const bootcamps = require('./routes/bootcamps');
+const bootcamps = require("./routes/bootcamps");
 
 // Load env variables
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
+
+// Connect To Database
+connectDB();
 
 const app = express();
 
-
-
 // Dev logging Middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgon('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgon("dev"));
 }
 
 // Mount routers
-app.use('/api/v1/bootcamps', bootcamps)
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`The server is running on ${PORT} and mode is ${process.env.NODE_ENV}`))
+const server = app.listen(
+  PORT,
+  console.log(
+    `The server is running on ${PORT} and mode is ${process.env.NODE_ENV}`
+  )
+);
+
+// Handle Unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close Server & Exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});
